d11a9891f51fb436be39f76ee0dd0914c3c054bc
lapis-overdose <lapis.zeymey@gmail.com>
2024-12-13 00:16:28 +0500

    added datetime, deleted sort

--- main.py
+++ main.py
@@ -1,28 +1,28 @@
+from datetime import datetime
+
+
+
 class TaskManager:
 
-    def __init__(self,name,priority,start,end,status):
+    def __init__(self,name,start,end,status):
         # atributes
         self.name = name
-        self.priority = priority
-        self.start = start
-        self.end = end
+        self.start = datetime.strptime(start, "%d,%m,%Y")
+        self.end = datetime.strptime(end, "%d,%m,%Y")
         self.status = status
 
         #УДАЛИТЕ pass КОГДА БУДЕТЕ ДЕЛАТЬ
 
-    def displayTask(self): #менюшка
+    def displayTasks(self): #менюшка
         pass
 
-    def settings(self): #удаляется *settings *priority_up , заменяется на *add *delete *back
+    def settings(self):
         pass
 
     def addTask(self):
         pass
 
     def deleteTask(self):
-        pass
-
-    def sortPriority(self): #только 2 приоритета low,high
         pass
 
 
