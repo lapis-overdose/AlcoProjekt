1bd5789a45ba724fe1ec71f888fbf6afc8892aa8
lapis-overdose <lapis.zeymey@gmail.com>
2024-12-12 17:13:06 +0500

    First model

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
-print("timur gaynius")
+class TaskManager:
+
+    def __init__(self,name,priority,start,end,status):
+        # atributes
+        self.name = name
+        self.priority = priority
+        self.start = start
+        self.end = end
+        self.status = status
+
+        #УДАЛИТЕ pass КОГДА БУДЕТЕ ДЕЛАТЬ
+
+    def displayTask(self): #менюшка
+        pass
+
+    def settings(self): #удаляется *settings *priority_up , заменяется на *add *delete *back
+        pass
+
+    def addTask(self):
+        pass
+
+    def deleteTask(self):
+        pass
+
+    def sortPriority(self): #только 2 приоритета low,high
+        pass
 
 
-
-print ("f")
